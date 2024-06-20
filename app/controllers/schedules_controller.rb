e1b13b35732c460bb54efe15c7343c124896de6d
jbdevelop3r <justinebanogon14@gmail.com>
2024-06-21 00:18:10 +0800

    working dynamic delete and create job, static input

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,16 +4,18 @@ class SchedulesController < ApplicationController
 
   def update
     interval = params[:interval].to_i
+    sched_name = params[:name]
     cron_expression = "*/#{interval} * * * *"
-    binding.b
+    id = 5
+    @sched = Schedule.find(id)
     job = Sidekiq::Cron::Job.find('my_job')
     if job
+      existing_job = Schedule.find_by(name: job.name)
+      existing_job.destroy
       job.destroy
     end
-    Sidekiq::Cron::Job.create(name: 'my_job', cron: cron_expression, class: 'MyWorker')
-    binding.b
+    Sidekiq::Cron::Job.create(name: sched_name, cron: cron_expression, class: 'MyWorker')
+    Schedule.create(name: sched_name, interval: interval)
     redirect_to edit_schedule_path, notice: "Schedule updated to every #{interval} minutes."
-    updated_job = Sidekiq::Cron::Job.find('my_job')
-    binding.b
   end
 end
